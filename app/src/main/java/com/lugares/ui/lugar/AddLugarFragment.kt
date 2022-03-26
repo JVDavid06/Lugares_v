812536dc7886c069fa964fe96be1d19d7ab5605a
JVDavid06 <djimenez130873@ufide.ac.cr>
2022-03-26 10:41:13 -0600

    Commit de la semana 9

--- app/src/main/java/com/lugares/ui/lugar/AddLugarFragment.kt
+++ app/src/main/java/com/lugares/ui/lugar/AddLugarFragment.kt
@@ -9,7 +9,6 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.core.app.ActivityCompat
 import androidx.lifecycle.ViewModelProvider
-import androidx.navigation.fragment.findNavController
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
 import com.lugares.R
@@ -17,13 +16,32 @@ import com.lugares.databinding.FragmentAddLugarBinding
 import com.lugares.model.Lugar
 import com.lugares.viewmodel.LugarViewModel
 import android.Manifest
+import android.app.Activity
+import android.content.Intent
 import android.location.Location
+import android.net.Uri
+import android.widget.ProgressBar
+import android.widget.TextView
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.navigation.fragment.findNavController
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.StorageReference
+import com.google.firebase.storage.ktx.storage
+import com.lugares.utiles.AudioUtiles
+import com.lugares.utiles.ImagenUtiles
 
 
 class AddLugarFragment : Fragment() {
     private lateinit var lugarViewModel: LugarViewModel
     private var _binding: FragmentAddLugarBinding? = null
     private val binding get() = _binding!!
+
+    private lateinit var audioUtiles: AudioUtiles
+
+    private lateinit var imagenUtiles: ImagenUtiles
+    private lateinit var tomarFotoActivity: ActivityResultLauncher<Intent>
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -35,13 +53,84 @@ class AddLugarFragment : Fragment() {
         _binding = FragmentAddLugarBinding.inflate(inflater, container, false)
 
         binding.btAddLugar.setOnClickListener{
-            agregarLugar()
+            binding.progressBar.visibility=ProgressBar.VISIBLE
+            binding.msgMensaje.text=getString(R.string.msg_subiendo_audio)
+            binding.msgMensaje.visibility = TextView.VISIBLE
+            subeAudioNube()
         }
+
+        audioUtiles= AudioUtiles(
+            requireActivity(),
+            requireContext(),
+            binding.btAccion,
+            binding.btPlay,
+            binding.btDelete,
+            getString(R.string.msg_graba_audio),
+            getString(R.string.msg_detener_audio)
+
+        )
+
+        tomarFotoActivity=registerForActivityResult(ActivityResultContracts.StartActivityForResult()){result ->
+            if(result.resultCode== Activity.RESULT_OK){
+                imagenUtiles.actualizaFoto()
+            }
+        }
+
+        imagenUtiles = ImagenUtiles(
+            requireContext(),
+            binding.btPhoto,
+            binding.btRotaL,
+            binding.btRotaR,
+            binding.imagen,
+            tomarFotoActivity)
 
         ubicaGPS()
 
         return binding.root
     }
+
+    private fun subeAudioNube() {
+        val audioFile = audioUtiles.audioFile
+        if(audioFile.exists() && audioFile.isFile && audioFile.canRead()){
+            val ruta = Uri.fromFile(audioFile)
+            val rutaNube="lugaresApp/${Firebase.auth.currentUser?.email}/audios/${audioFile.name}"
+
+            val referencia: StorageReference=Firebase.storage.reference.child(rutaNube)
+            referencia.putFile(ruta).addOnSuccessListener {
+                referencia.downloadUrl.addOnSuccessListener {
+                    val rutaAudio = it.toString()
+                    subeImagenNube(rutaAudio, "")
+                }
+            }
+                .addOnFailureListener{
+                    subeImagenNube("", "")
+                }
+        }else{
+            subeImagenNube("", "")
+        }
+    }
+
+    private fun subeImagenNube(rutaAudio: String, s: String) {
+        val imagenFile = imagenUtiles.imagenFile
+        if(imagenFile.exists() && imagenFile.isFile && imagenFile.canRead()){
+            val ruta = Uri.fromFile(imagenFile)
+            val rutaNube="lugaresApp/${Firebase.auth.currentUser?.email}/imagenes/${imagenFile.name}"
+
+            val referencia: StorageReference=Firebase.storage.reference.child(rutaNube)
+            referencia.putFile(ruta).addOnSuccessListener {
+                referencia.downloadUrl.addOnSuccessListener {
+                    val rutaImagen = it.toString()
+                    agregarLugar(rutaAudio,rutaImagen)
+                }
+            }
+                .addOnFailureListener{
+                    subeImagenNube(rutaAudio,"")
+                }
+        }else{
+            subeImagenNube(rutaAudio,"")
+        }
+    }
+
 
     private var conPermisos:Boolean=true
     private fun ubicaGPS() {
@@ -72,7 +161,7 @@ class AddLugarFragment : Fragment() {
         }
     }
 
-    private fun agregarLugar() {
+    private fun agregarLugar(rutaAudio: String, rutaImagen: String) {
         val nombre=binding.etNombre.text.toString()
         if (nombre.isNotEmpty()){
             val correo=binding.etCorreo.text.toString()
@@ -82,8 +171,9 @@ class AddLugarFragment : Fragment() {
             val longitud = binding.tvLongitud.text.toString().toDouble()
             val altura = binding.tvAltura.text.toString().toDouble()
 
-            val lugar= Lugar("",nombre,correo,telefono,web,latitud,longitud,altura,"", "")
+            val lugar= Lugar("",nombre,correo,telefono,web,latitud,longitud,altura,rutaAudio,rutaImagen)
             lugarViewModel.addLugar(lugar)
+
             Toast.makeText(requireContext(),getString(R.string.msg_lugar_add),Toast.LENGTH_SHORT).show()
             findNavController().navigate(R.id.action_addLugarFragment_to_nav_lugar)
         }
--- app/src/main/java/com/lugares/ui/lugar/UpdateLugarFragment.kt
+++ app/src/main/java/com/lugares/ui/lugar/UpdateLugarFragment.kt
@@ -4,6 +4,7 @@ import android.Manifest
 import android.app.AlertDialog
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.media.MediaPlayer
 import android.net.Uri
 import android.os.Bundle
 import android.view.*
@@ -12,6 +13,7 @@ import android.widget.Toast
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
+import com.bumptech.glide.Glide
 import com.lugares.R
 import com.lugares.databinding.FragmentUpdateLugarBinding
 import com.lugares.model.Lugar
@@ -23,6 +25,8 @@ class UpdateLugarFragment : Fragment() {
     private val binding get() = _binding!!
 
     private val args by navArgs<UpdateLugarFragmentArgs>()
+
+    private lateinit var mediaPlayer: MediaPlayer
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -47,6 +51,20 @@ class UpdateLugarFragment : Fragment() {
         binding.btWhatsapp.setOnClickListener{ enviarWhatsapp() }
         binding.btWeb.setOnClickListener{ verWeb() }
         binding.btLocation.setOnClickListener{ verMapa() }
+
+        if(args.lugar.rutaAudio?.isNotEmpty()==true){
+            mediaPlayer=MediaPlayer()
+            mediaPlayer.setDataSource(args.lugar.rutaAudio)
+            mediaPlayer.prepare()
+            binding.btPlay.isEnabled=true
+            binding.btPlay.setOnClickListener{mediaPlayer.start()}
+        }else{
+            binding.btPlay.isEnabled=false
+        }
+        if(args.lugar.rutaImagen?.isNotEmpty()==true){
+            Glide.with(requireContext()).load(args.lugar.rutaImagen).fitCenter().into(binding.imagen)
+        }
+
 
         setHasOptionsMenu(true) //este lo que dice es fragmento tiene un menu adicional
         return binding.root
@@ -158,7 +176,7 @@ class UpdateLugarFragment : Fragment() {
             val correo=binding.etCorreo.text.toString()
             val telefono=binding.etTelefono.text.toString()
             val web=binding.etWeb.text.toString()
-            val lugar= Lugar(args.lugar.id,nombre,correo,telefono,web,args.lugar.latitud,args.lugar.longitud,args.lugar.altura,"","")
+            val lugar= Lugar(args.lugar.id,nombre,correo,telefono,web,args.lugar.latitud,args.lugar.longitud,args.lugar.altura, args.lugar.rutaAudio,args.lugar.rutaImagen)
             lugarViewModel.updateLugar(lugar)
             Toast.makeText(requireContext(),getString(R.string.msg_lugar_update),Toast.LENGTH_SHORT).show()
             findNavController().navigate(R.id.action_nav_updateLugar_to_nav_lugar)
